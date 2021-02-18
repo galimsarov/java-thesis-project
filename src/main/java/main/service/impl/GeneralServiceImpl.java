6b8a5e13f022ef110b8b190e87a2b1af7754ba8e
Evgenii <e.galimsarov@gmail.com>
2021-02-18 17:45:32 +0500

    Images are saved in the project folder

--- src/main/java/main/service/impl/GeneralServiceImpl.java
+++ src/main/java/main/service/impl/GeneralServiceImpl.java
@@ -64,6 +64,48 @@ public class GeneralServiceImpl implements GeneralService {
      * Метод imageUpload
      * Метод загружает на сервер изображение в папку upload
      */
+//    @Override
+//    public Object imageUpload(MultipartFile file) throws IOException {
+//        String response = null;
+//        Error errorResponse = new Error();
+//        errorResponse.setResult(false);
+//        ImageResponse errors = new ImageResponse();
+//        if (!file.getOriginalFilename().endsWith("jpg") &&
+//                !file.getOriginalFilename().endsWith("png"))
+//            errors.setImage("Файл не является изображением");
+//        if (file.getSize() > 1048576)
+//            errors.setImage("Размер файла превышает допустимый размер");
+//        if (errors.getImage() != null) {
+//            errorResponse.setErrors(errors);
+//            return errorResponse;
+//        }
+//        if (!file.isEmpty()) {
+//            String uploadsDir = "/upload/";
+//            String realPathToUploads = request.getServletContext()
+//                    .getRealPath(uploadsDir);
+//            String workPiece = "abcdefghijklmnopqrstuvwxyz";
+//            String[] subDirs = new String[3];
+//            for (int i = 0; i < 3; i++) {
+//                Random random = new Random();
+//                int range = random.nextInt(25);
+//                subDirs[i] = workPiece.substring(range, range + 2);
+//            }
+//            if (!new File(realPathToUploads).exists())
+//                new File(realPathToUploads).mkdir();
+//            for (String subDir : subDirs) {
+//                realPathToUploads += subDir + "/";
+//                if (!new File(realPathToUploads).exists())
+//                    new File(realPathToUploads).mkdir();
+//                uploadsDir += subDir + "/";
+//            }
+//            String orgName = file.getOriginalFilename();
+//            String filePath = realPathToUploads + orgName;
+//            File dest = new File(filePath);
+//            file.transferTo(dest);
+//            response = uploadsDir + file.getOriginalFilename();
+//        }
+//        return response;
+//    }
     @Override
     public Object imageUpload(MultipartFile file) throws IOException {
         String response = null;
@@ -80,9 +122,14 @@ public class GeneralServiceImpl implements GeneralService {
             return errorResponse;
         }
         if (!file.isEmpty()) {
-            String uploadsDir = "/upload/";
-            String realPathToUploads = request.getServletContext()
-                    .getRealPath(uploadsDir);
+            String realPathToUploads = request.getServletContext().getRealPath("")
+                    + file.getOriginalFilename();
+            File dest = new File(realPathToUploads);
+            file.transferTo(dest);
+
+            String uploadsDir = "src/main/resources/static/img/upload/";
+            if (!new File(uploadsDir).exists())
+                new File(uploadsDir).mkdir();
             String workPiece = "abcdefghijklmnopqrstuvwxyz";
             String[] subDirs = new String[3];
             for (int i = 0; i < 3; i++) {
@@ -90,19 +137,16 @@ public class GeneralServiceImpl implements GeneralService {
                 int range = random.nextInt(25);
                 subDirs[i] = workPiece.substring(range, range + 2);
             }
-            if (!new File(realPathToUploads).exists())
-                new File(realPathToUploads).mkdir();
             for (String subDir : subDirs) {
-                realPathToUploads += subDir + "/";
-                if (!new File(realPathToUploads).exists())
-                    new File(realPathToUploads).mkdir();
                 uploadsDir += subDir + "/";
+                if (!new File(uploadsDir).exists())
+                    new File(uploadsDir).mkdir();
             }
-            String orgName = file.getOriginalFilename();
-            String filePath = realPathToUploads + orgName;
-            File dest = new File(filePath);
-            file.transferTo(dest);
-            response = uploadsDir + file.getOriginalFilename();
+            BufferedImage image = ImageIO.read(dest);
+            File newFile = new File(uploadsDir + file.getOriginalFilename());
+            ImageIO.write(image, "jpg", newFile);
+
+            response = uploadsDir.substring(25) + file.getOriginalFilename();
         }
         return response;
     }
@@ -334,27 +378,8 @@ public class GeneralServiceImpl implements GeneralService {
             if (requestWithPhoto.getPassword() != null)
                 user.setPassword(requestWithPhoto.getPassword());
 
-            String uploadsDir = "/upload/";
             String realPathToUploads = request.getServletContext()
-                    .getRealPath(uploadsDir);
-
-            String workPiece = "abcdefghijklmnopqrstuvwxyz";
-            String[] subDirs = new String[3];
-            for (int i = 0; i < 3; i++) {
-                Random random = new Random();
-                int range = random.nextInt(25);
-                subDirs[i] = workPiece.substring(range, range + 2);
-            }
-
-            if (!new File(realPathToUploads).exists())
-                new File(realPathToUploads).mkdir();
-
-            for (String subDir : subDirs) {
-                realPathToUploads += subDir + "/";
-                if (!new File(realPathToUploads).exists())
-                    new File(realPathToUploads).mkdir();
-                uploadsDir += subDir + "/";
-            }
+                    .getRealPath("");
             String orgName = requestWithPhoto.getPhoto().getOriginalFilename();
             String filePath = realPathToUploads + orgName;
 
