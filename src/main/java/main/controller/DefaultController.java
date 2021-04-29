c5d6ac2d1dd97294e957835c95ac67ac074a5010
Evgenii <e.galimsarov@gmail.com>
2021-04-29 15:53:46 +0300

    Fixed DefaultController

--- src/main/java/main/controller/DefaultController.java
+++ src/main/java/main/controller/DefaultController.java
@@ -1,11 +1,22 @@
 package main.controller;
 
+import main.utils.MediaTypeUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.ServletContext;
+import java.io.FileInputStream;
+import java.io.IOException;
 
 @Controller
 public class DefaultController {
+    @Autowired
+    private ServletContext servletContext;
+
     @RequestMapping("/")
     public String index() {
         return "index";
@@ -16,5 +27,12 @@ public class DefaultController {
             value = "/**/{path:[^\\.]*}")
     public String redirectToIndex() {
         return "index";
+    }
+
+    @GetMapping("/img")
+    public ResponseEntity<InputStreamResource> getImage(@PathVariable String fileName) throws IOException {
+        MediaType mediaType = MediaTypeUtils.getMediaTypeForFileName(this.servletContext, fileName);
+        InputStreamResource resource = new InputStreamResource(new FileInputStream(fileName));
+        return ResponseEntity.ok().contentType(mediaType).body(resource);
     }
 }
